9f898f449f3f8609d5be145868c1e4fb659c9b1a
dungad2k2 <dung9d32k2@gmail.com>
2024-06-10 11:06:57 +0000

    update

--- main.py
+++ main.py
@@ -1,53 +1,53 @@
 from flask import request, jsonify
 from config import app, db
-from models import Contact
+from models import Student
 
-@app.route("/contacts", methods=["GET"])
-def get_contacts():
-    contacts = Contact.query.all()
-    json_contacts = list(map(lambda x: x.to_json(), contacts))
-    return jsonify({"contacts": json_contacts})
-@app.route("/create_contact", methods=["POST"])
-def create_contacts():
-    first_name = request.json.get("firstName")
-    last_name = request.json.get("lastName")
-    email = request.json.get("email")
+@app.route("/students", methods=["GET"])
+def get_students():
+    students = Student.query.all()
+    json_students = list(map(lambda x: x.to_json(), students))
+    return jsonify({"students": json_students})
+@app.route("/create_student", methods=["POST"])
+def create_students():
+    fullname = request.json.get("fullName")
+    gender = request.json.get("gender")
+    school = request.json.get("school")
 
-    if not first_name or not last_name or not email:
+    if not fullname or not gender or not school:
         return (jsonify ({"message": "You must provide all essential information"}),400,)
-    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    new_student = Student(fullname=fullname, gender=gender, school=school)
     try: 
-        db.session.add(new_contact)
+        db.session.add(new_student)
         db.session.commit()
     except Exception as e:
         return jsonify ({"message": str(e)}), 400
-    return jsonify({"message": "User created!"}), 201
-@app.route("/update_contact/<int:user_id>", methods=["PATCH"])
-def update_contacts(user_id):
-    contact = Contact.query.get(user_id)
+    return jsonify({"message": "Student created!"}), 201
+@app.route("/update_student/<int:user_id>", methods=["PATCH"])
+def update_students(user_id):
+    student = Student.query.get(user_id)
 
-    if not contact:
-        return jsonify({"message": "Contact Not Found!"}), 404
+    if not student:
+        return jsonify({"message": "Student Not Found!"}), 404
     
     data = request.json
-    contact.first_name = data.get("firstName", contact.first_name)
-    contact.last_name = data.get("lastName", contact.last_name)
-    contact.email = data.get("email", contact.email)
+    student.fullname = data.get("fullName", student.fullname)
+    student.gender = data.get("gender", student.gender)
+    student.school = data.get("school", student.school)
 
     db.session.commit()
 
-    return jsonify({"message": "Contact updated!"}), 200
-@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
-def delete_contact(user_id):
-    contact = Contact.query.get(user_id)
+    return jsonify({"message": "Student updated!"}), 200
+@app.route("/delete_student/<int:user_id>", methods=["DELETE"])
+def delete_student(user_id):
+    student = Student.query.get(user_id)
 
-    if not contact:
+    if not student:
         return jsonify({"message": "user not found"}), 404
-    db.session.delete(contact)
+    db.session.delete(student)
     db.session.commit()
-    return  jsonify({"message": "Contact deleted!"})
+    return  jsonify({"message": "Student deleted!"})
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all() #
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
